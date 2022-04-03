396e49594e08b116c2ff6bb3b70f89e34e8a814c
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-04-03 20:34:10 +0100

    Feat: Notification

--- main.js
+++ main.js
@@ -1,23 +1,33 @@
-const { BrowserWindow, app } = require('electron');
+
+const { BrowserWindow, app, ipcMain, Notification } = require('electron');
 const path = require('path');
+
+const isDev = !app.isPackaged;
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    backgroundColor: '#ffffff',
+    width: 1200,
+    height: 800,
+    backgroundColor: "white",
     webPreferences: {
       nodeIntegration: false,
       worldSafeExecuteJavaScript: true,
-      contextIsolation: true
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js')
     }
   })
 
   win.loadFile('index.html');
 }
 
-require('electron-reload')(__dirname, {
-  electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
-});
+if (isDev) {
+  require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+  })
+}
 
-app.whenReady().then(createWindow);
+ipcMain.on('notify', (_, message) => {
+  new Notification({title: 'Notifiation', body: message}).show();
+})
+
+app.whenReady().then(createWindow)
--- src/js/App.js
+++ src/js/App.js
@@ -4,7 +4,9 @@ export default function App () {
   return (
     <>
       <h1>Hello Electron from component App</h1>
-      <button>Notify me</button>
+      <button onClick={() => {
+        electron.notificationApi.sendNotification('Hello from electron')
+      }}>Notify me</button>
     </>
   )
 }
